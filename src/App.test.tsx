43f94fbb9513399714b445d8ce6d07f28217b388
Artyom Pizin <artempetr2016@gmail.com>
2025-01-06 15:22:00 +0300

    test(App): add a test for displaying the preview box

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+
+import App from '@/App';
+
+describe('App', () => {
+  it('should show the preview box to which the border-radius is applied', () => {
+    render(<App />);
+
+    expect(screen.getByLabelText(/preview box/i)).toBeInTheDocument();
+  });
+});
