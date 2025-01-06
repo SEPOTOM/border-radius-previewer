6c42a21eef19ec6af7527904b546d3c3f0fbbdf8
Artyom Pizin <artempetr2016@gmail.com>
2025-01-06 15:44:33 +0300

    feat(App): display inputs for rounding off the corners

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 const App = () => (
-  <div
-    role="presentation"
-    aria-label="Preview box"
-    className="h-24 w-24 rounded-full bg-main"
-  />
+  <>
+    <input type="number" aria-label="Top-left corner" />
+    <input type="number" aria-label="Top-right corner" />
+    <div
+      role="presentation"
+      aria-label="Preview box"
+      className="h-24 w-24 rounded-full bg-main"
+    />
+    <input type="number" aria-label="Bottom-left corner" />
+    <input type="number" aria-label="Bottom-right corner" />
+  </>
 );
 
 export default App;
