2ef7f4369af26433bb00dcd34351591b279ef6e5
Artyom Pizin <artempetr2016@gmail.com>
2025-01-06 15:28:19 +0300

    feat(App): display the preview box

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
-const App = () => null;
+const App = () => (
+  <div
+    role="presentation"
+    aria-label="Preview box"
+    className="h-24 w-24 rounded-full bg-main"
+  />
+);
 
 export default App;
