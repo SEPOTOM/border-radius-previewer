b74344513fd45bc142981a917c4297d1f67149a4
Artyom Pizin <artempetr2016@gmail.com>
2025-01-06 16:26:26 +0300

    feat(App): add default value of 0 to the inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 const App = () => (
   <>
-    <input type="number" aria-label="Top-left corner" />
-    <input type="number" aria-label="Top-right corner" />
+    <input type="number" aria-label="Top-left corner" value={0} />
+    <input type="number" aria-label="Top-right corner" value={0} />
     <div
       role="presentation"
       aria-label="Preview box"
       className="h-24 w-24 bg-main"
       style={{ borderRadius: '0 0 0 0' }}
     />
-    <input type="number" aria-label="Bottom-left corner" />
-    <input type="number" aria-label="Bottom-right corner" />
+    <input type="number" aria-label="Bottom-left corner" value={0} />
+    <input type="number" aria-label="Bottom-right corner" value={0} />
   </>
 );
 
