30b2d6aef5aa91f52d312b158a1b232ba52a1260
Artyom Pizin <artempetr2016@gmail.com>
2025-01-06 15:42:47 +0300

    test(App): add tests for displaying inputs for rounding off the corners

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,4 +8,20 @@ describe('App', () => {
 
     expect(screen.getByLabelText(/preview box/i)).toBeInTheDocument();
   });
+
+  describe('should show inputs for rounding off the corners', () => {
+    const corners = ['top-left', 'top-right', 'bottom-left', 'bottom-right'];
+
+    corners.forEach((corner) => {
+      it(corner, () => {
+        render(<App />);
+
+        expect(
+          screen.getByRole('spinbutton', {
+            name: new RegExp(`${corner} corner`, 'i'),
+          }),
+        ).toBeInTheDocument();
+      });
+    });
+  });
 });
