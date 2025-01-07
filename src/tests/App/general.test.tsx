ef2523353a7cb6f7ad99891ed08e92812f447742
Artyom Pizin <artempetr2016@gmail.com>
2025-01-07 19:21:23 +0300

    refactor(tests): separate tests for better structuring

--- src/tests/App/general.test.tsx
+++ src/tests/App/general.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 
 import App from '@/App';
-import { renderWithUser } from '@/tests';
+import { renderWithUser } from '@/tests/utils';
 
 describe('App', () => {
   it('should show the preview box to which the border-radius is applied', () => {
@@ -55,56 +55,5 @@ describe('App', () => {
     const previewBoxStyle = getComputedStyle(previewBox);
 
     expect(previewBoxStyle.borderRadius).toBe('0 0 0 0');
-  });
-});
-
-describe("App's inputs for rounding off the corners", () => {
-  const corners = ['top-left', 'top-right', 'bottom-right', 'bottom-left'];
-
-  it('should be displayed', () => {
-    render(<App />);
-
-    corners.forEach((corner) => {
-      expect(
-        screen.getByRole('spinbutton', {
-          name: new RegExp(`${corner} corner`, 'i'),
-        }),
-      ).toBeInTheDocument();
-    });
-  });
-
-  it('should have an initial value of 0', () => {
-    render(<App />);
-
-    corners.forEach((corner) => {
-      expect(
-        screen.getByRole('spinbutton', {
-          name: new RegExp(`${corner} corner`, 'i'),
-        }),
-      ).toHaveDisplayValue('0');
-    });
-  });
-
-  describe('should round off the appropriate corner', () => {
-    const values = ['25', '33', '12', '100'];
-
-    corners.forEach((corner, index) => {
-      it(corner, async () => {
-        const { user } = renderWithUser(<App />);
-
-        await user.type(
-          screen.getByRole('spinbutton', { name: new RegExp(corner, 'i') }),
-          values[index],
-        );
-
-        const previewBox = screen.getByLabelText(/preview box/i);
-        const previewBoxStyle = getComputedStyle(previewBox);
-        const borderRadiusValues = previewBoxStyle.borderRadius.split(' ');
-
-        expect(borderRadiusValues[index]).toMatch(
-          new RegExp(values[index], 'i'),
-        );
-      });
-    });
   });
 });
