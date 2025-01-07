ac6eb291de82acdf44cac3fc396c884a22b1c3a0
Artyom Pizin <artempetr2016@gmail.com>
2025-01-07 13:10:32 +0300

    test(App): add tests for rounding off each corner via corresponding input

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 
 import App from '@/App';
+import { renderWithUser } from '@/tests';
 
 describe('App', () => {
   it('should show the preview box to which the border-radius is applied', () => {
@@ -19,7 +20,7 @@ describe('App', () => {
 });
 
 describe("App's inputs for rounding off the corners", () => {
-  const corners = ['top-left', 'top-right', 'bottom-left', 'bottom-right'];
+  const corners = ['top-left', 'top-right', 'bottom-right', 'bottom-left'];
 
   it('should be displayed', () => {
     render(<App />);
@@ -42,6 +43,29 @@ describe("App's inputs for rounding off the corners", () => {
           name: new RegExp(`${corner} corner`, 'i'),
         }),
       ).toHaveDisplayValue('0');
+    });
+  });
+
+  describe('should round off the appropriate corner', () => {
+    const values = ['25', '33', '12', '100'];
+
+    corners.forEach((corner, index) => {
+      it(corner, async () => {
+        const { user } = renderWithUser(<App />);
+
+        await user.type(
+          screen.getByRole('spinbutton', { name: new RegExp(corner, 'i') }),
+          values[index],
+        );
+
+        const previewBox = screen.getByLabelText(/preview box/i);
+        const previewBoxStyle = getComputedStyle(previewBox);
+        const borderRadiusValues = previewBoxStyle.borderRadius.split(' ');
+
+        expect(borderRadiusValues[index]).toMatch(
+          new RegExp(values[index], 'i'),
+        );
+      });
     });
   });
 });
