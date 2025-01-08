17659b37e8e5ddf13a4e2c1ec54ad7797c852973
Artyom Pizin <artempetr2016@gmail.com>
2025-01-08 15:27:11 +0300

    feat(App): add styles

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,13 @@ const App = () => {
   };
 
   return (
-    <>
+    <div className="mx-auto flex h-full max-w-5xl flex-col justify-center gap-y-3 p-3">
       <button
         type="button"
         role="switch"
         aria-checked={isAdvanced}
         onClick={handleModesSwitch}
+        className="mx-auto rounded-md border-4 border-main p-2 text-xl font-black uppercase text-main transition-colors hover:bg-main hover:text-secondary active:border-main-dark active:bg-main-dark active:text-secondary active:transition-none"
       >
         Switch the mode
       </button>
@@ -40,7 +41,7 @@ const App = () => {
           <div
             role="presentation"
             aria-label="Preview box"
-            className="h-24 w-24 bg-main"
+            className="mx-auto h-72 w-72 bg-main transition-all"
             style={{ borderRadius: advancedBorderRadius }}
           />
         </AdvancedModeControls>
@@ -48,16 +49,20 @@ const App = () => {
           <div
             role="presentation"
             aria-label="Preview box"
-            className="h-24 w-24 bg-main"
-            style={{ borderRadius }}
+            className="mx-auto h-72 w-72 bg-main transition-all"
+            style={{ borderRadius: borderRadius }}
           />
         </NormalModeControls>
       }
 
-      <button type="button" onClick={() => void handleCopyButtonClick()}>
+      <button
+        type="button"
+        onClick={() => void handleCopyButtonClick()}
+        className="mx-auto rounded-md border-4 border-main p-2 text-xl font-black uppercase text-main transition-colors hover:bg-main hover:text-secondary active:border-main-dark active:bg-main-dark active:text-secondary active:transition-none"
+      >
         Copy to clipboard!
       </button>
-    </>
+    </div>
   );
 };
 
