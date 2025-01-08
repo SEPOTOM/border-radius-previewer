e471428d3af0676b0329b756bc340e692067dfdb
Artyom Pizin <artempetr2016@gmail.com>
2025-01-08 13:01:49 +0300

    refactor(tests): remove redundant nesting

--- src/tests/App/advancedMode.test.tsx
+++ src/tests/App/advancedMode.test.tsx
@@ -3,45 +3,43 @@ import { screen } from '@testing-library/react';
 import App from '@/App';
 import { renderWithUser } from '@/tests/utils';
 
-describe('App in advanced mode', () => {
-  describe('inputs for specifying corresponding radii', () => {
-    const radii = [
-      'horizontal top-left',
-      'horizontal top-right',
-      'horizontal bottom-left',
-      'horizontal bottom-right',
-      'vertical top-left',
-      'vertical top-right',
-      'vertical bottom-left',
-      'vertical bottom-right',
-    ];
+describe('inputs for specifying corresponding radii', () => {
+  const radii = [
+    'horizontal top-left',
+    'horizontal top-right',
+    'horizontal bottom-left',
+    'horizontal bottom-right',
+    'vertical top-left',
+    'vertical top-right',
+    'vertical bottom-left',
+    'vertical bottom-right',
+  ];
 
-    it('should be displayed', async () => {
-      const { user } = renderWithUser(<App />);
+  it('should be displayed in advanced mode', async () => {
+    const { user } = renderWithUser(<App />);
 
-      await user.click(screen.getByRole('switch', { name: /mode/i }));
+    await user.click(screen.getByRole('switch', { name: /mode/i }));
 
-      radii.forEach((radius) => {
-        expect(
-          screen.getByRole('spinbutton', {
-            name: new RegExp(`${radius} radius`, 'i'),
-          }),
-        ).toBeInTheDocument();
-      });
+    radii.forEach((radius) => {
+      expect(
+        screen.getByRole('spinbutton', {
+          name: new RegExp(`${radius} radius`, 'i'),
+        }),
+      ).toBeInTheDocument();
     });
+  });
 
-    it('should have an initial value of 0', async () => {
-      const { user } = renderWithUser(<App />);
+  it('should have an initial value of 0', async () => {
+    const { user } = renderWithUser(<App />);
 
-      await user.click(screen.getByRole('switch', { name: /mode/i }));
+    await user.click(screen.getByRole('switch', { name: /mode/i }));
 
-      radii.forEach((radius) => {
-        expect(
-          screen.getByRole('spinbutton', {
-            name: new RegExp(`${radius} radius`, 'i'),
-          }),
-        ).toHaveDisplayValue('0');
-      });
+    radii.forEach((radius) => {
+      expect(
+        screen.getByRole('spinbutton', {
+          name: new RegExp(`${radius} radius`, 'i'),
+        }),
+      ).toHaveDisplayValue('0');
     });
   });
 });
--- src/tests/App/normalMode.test.tsx
+++ src/tests/App/normalMode.test.tsx
@@ -3,54 +3,52 @@ import { render, screen } from '@testing-library/react';
 import App from '@/App';
 import { renderWithUser } from '@/tests/utils';
 
-describe('App in normal mode', () => {
-  describe('inputs for rounding off the corners', () => {
-    const corners = ['top-left', 'top-right', 'bottom-right', 'bottom-left'];
+describe('inputs for rounding off the corners', () => {
+  const corners = ['top-left', 'top-right', 'bottom-right', 'bottom-left'];
 
-    it('should be displayed', () => {
-      render(<App />);
+  it('should be displayed in normal mode', () => {
+    render(<App />);
 
-      corners.forEach((corner) => {
-        expect(
-          screen.getByRole('spinbutton', {
-            name: new RegExp(`${corner} corner`, 'i'),
-          }),
-        ).toBeInTheDocument();
-      });
+    corners.forEach((corner) => {
+      expect(
+        screen.getByRole('spinbutton', {
+          name: new RegExp(`${corner} corner`, 'i'),
+        }),
+      ).toBeInTheDocument();
     });
+  });
 
-    it('should have an initial value of 0', () => {
-      render(<App />);
+  it('should have an initial value of 0', () => {
+    render(<App />);
 
-      corners.forEach((corner) => {
-        expect(
-          screen.getByRole('spinbutton', {
-            name: new RegExp(`${corner} corner`, 'i'),
-          }),
-        ).toHaveDisplayValue('0');
-      });
+    corners.forEach((corner) => {
+      expect(
+        screen.getByRole('spinbutton', {
+          name: new RegExp(`${corner} corner`, 'i'),
+        }),
+      ).toHaveDisplayValue('0');
     });
+  });
 
-    describe('should round off the appropriate corner', () => {
-      const values = ['25', '33', '12', '100'];
+  describe('should round off the appropriate corner', () => {
+    const values = ['25', '33', '12', '100'];
 
-      corners.forEach((corner, index) => {
-        it(corner, async () => {
-          const { user } = renderWithUser(<App />);
+    corners.forEach((corner, index) => {
+      it(corner, async () => {
+        const { user } = renderWithUser(<App />);
 
-          await user.type(
-            screen.getByRole('spinbutton', { name: new RegExp(corner, 'i') }),
-            values[index],
-          );
+        await user.type(
+          screen.getByRole('spinbutton', { name: new RegExp(corner, 'i') }),
+          values[index],
+        );
 
-          const previewBox = screen.getByLabelText(/preview box/i);
-          const previewBoxStyle = getComputedStyle(previewBox);
-          const borderRadiusValues = previewBoxStyle.borderRadius.split(' ');
+        const previewBox = screen.getByLabelText(/preview box/i);
+        const previewBoxStyle = getComputedStyle(previewBox);
+        const borderRadiusValues = previewBoxStyle.borderRadius.split(' ');
 
-          expect(borderRadiusValues[index]).toMatch(
-            new RegExp(values[index], 'i'),
-          );
-        });
+        expect(borderRadiusValues[index]).toMatch(
+          new RegExp(values[index], 'i'),
+        );
       });
     });
   });
