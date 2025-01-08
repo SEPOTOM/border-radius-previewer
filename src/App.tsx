d07c97f86ab75893cae4f25c6e4c8214de56d56a
Artyom Pizin <artempetr2016@gmail.com>
2025-01-08 13:47:58 +0300

    feat(App): add the ability to update the values of radii in advanced mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 import { NormalModeControls } from '@/components';
 
 const App = () => {
   const [borderRadius, setBorderRadius] = useState('0 0 0 0');
+  const [advancedBorderRadiusValues, setAdvancedBorderRadiusValues] = useState({
+    'horz-top-left': '0',
+    'horz-top-right': '0',
+    'horz-bottom-right': '0',
+    'horz-bottom-left': '0',
+    'vert-top-left': '0',
+    'vert-top-right': '0',
+    'vert-bottom-right': '0',
+    'vert-bottom-left': '0',
+  });
   const [isAdvanced, setIsAdvanced] = useState(false);
+
+  const handleAdvancedBorderRadiusValuesChange = (
+    e: ChangeEvent<HTMLInputElement>,
+  ) => {
+    setAdvancedBorderRadiusValues({
+      ...advancedBorderRadiusValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const advancedBorderRadius = [
+    advancedBorderRadiusValues['horz-top-left'],
+    advancedBorderRadiusValues['horz-top-right'],
+    advancedBorderRadiusValues['horz-bottom-right'],
+    advancedBorderRadiusValues['horz-bottom-left'],
+    advancedBorderRadiusValues['vert-top-left'],
+    advancedBorderRadiusValues['vert-top-right'],
+    advancedBorderRadiusValues['vert-bottom-right'],
+    advancedBorderRadiusValues['vert-bottom-left'],
+  ].map((value) => {
+    const parsedValue = parseFloat(value);
+
+    if (isNaN(parsedValue) || parsedValue === 0) {
+      return '0';
+    } else {
+      return `${parsedValue}px`;
+    }
+  });
+
+  advancedBorderRadius.splice(4, 0, '/');
 
   const handleBorderRadiusChange = (newBorderRadius: string) => {
     setBorderRadius(newBorderRadius);
@@ -34,48 +74,64 @@ const App = () => {
           <input
             type="number"
             aria-label="Horizontal top-left radius"
-            value={0}
+            value={advancedBorderRadiusValues['horz-top-left']}
+            name="horz-top-left"
+            onChange={handleAdvancedBorderRadiusValuesChange}
           />
           <input
             type="number"
             aria-label="Horizontal top-right radius"
-            value={0}
+            value={advancedBorderRadiusValues['horz-top-right']}
+            name="horz-top-right"
+            onChange={handleAdvancedBorderRadiusValuesChange}
           />
           <input
             type="number"
             aria-label="Horizontal bottom-left radius"
-            value={0}
+            value={advancedBorderRadiusValues['horz-bottom-left']}
+            name="horz-bottom-left"
+            onChange={handleAdvancedBorderRadiusValuesChange}
           />
           <input
             type="number"
             aria-label="Horizontal bottom-right radius"
-            value={0}
+            value={advancedBorderRadiusValues['horz-bottom-right']}
+            name="horz-bottom-right"
+            onChange={handleAdvancedBorderRadiusValuesChange}
           />
           <div
             role="presentation"
             aria-label="Preview box"
             className="h-24 w-24 bg-main"
-            style={{ borderRadius: '0 0 0 0 / 0 0 0 0' }}
+            style={{ borderRadius: advancedBorderRadius.join(' ') }}
           />
           <input
             type="number"
             aria-label="Vertical top-left radius"
-            value={0}
+            value={advancedBorderRadiusValues['vert-top-left']}
+            name="vert-top-left"
+            onChange={handleAdvancedBorderRadiusValuesChange}
           />
           <input
             type="number"
             aria-label="Vertical top-right radius"
-            value={0}
+            value={advancedBorderRadiusValues['vert-top-right']}
+            name="vert-top-right"
+            onChange={handleAdvancedBorderRadiusValuesChange}
           />
           <input
             type="number"
             aria-label="Vertical bottom-left radius"
-            value={0}
+            value={advancedBorderRadiusValues['vert-bottom-left']}
+            name="vert-bottom-left"
+            onChange={handleAdvancedBorderRadiusValuesChange}
           />
           <input
             type="number"
             aria-label="Vertical bottom-right radius"
-            value={0}
+            value={advancedBorderRadiusValues['vert-bottom-right']}
+            name="vert-bottom-right"
+            onChange={handleAdvancedBorderRadiusValuesChange}
           />
         </>
       : <NormalModeControls onBorderRadiusChange={handleBorderRadiusChange}>
