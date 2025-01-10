540035a8a4a8a4a9bb03bab08619470aa2c0a70d
Artyom Pizin <artempetr2016@gmail.com>
2025-01-10 16:41:28 +0300

    feat(NormalModeControls): update styles and layout to place unit dropdowns

--- src/components/NormalModeControls/index.tsx
+++ src/components/NormalModeControls/index.tsx
@@ -28,74 +28,78 @@ const NormalModeControls: FC<NormalModeControlsProps> = ({
   return (
     <>
       <div className="mx-auto flex w-full max-w-[432px] justify-between overflow-hidden sm:justify-center sm:gap-36">
-        <div className="max-w-36 shrink-0 grow-0 basis-1/2 pr-2">
+        <div className="flex max-w-36 shrink-0 grow-0 basis-1/2 pr-2">
           <input
             type="number"
             aria-label="Top-left corner"
             name="top-left"
             value={borderRadiusValues['top-left']}
             onChange={handleBorderRadiusChange}
-            className="outline-focus w-full max-w-full rounded-sm bg-main p-1 text-xl tracking-wider text-secondary"
+            className="outline-focus w-full max-w-full rounded-l-sm rounded-r-none border-r border-dashed border-secondary bg-main p-1 text-xl tracking-wider text-secondary"
+          />
+          <UnitDropdown
+            aria-label="Top-left corner unit"
+            name="top-left-unit"
+            onChange={handleBorderRadiusUnitChange}
+            value={borderRadiusUnits['top-left-unit']}
+            className="rounded-l-none rounded-r-sm border-l border-dashed border-secondary bg-main p-1 text-xl tracking-wider text-secondary"
           />
         </div>
-        <div className="max-w-36 shrink-0 grow-0 basis-1/2 pl-2">
+        <div className="flex max-w-36 shrink-0 grow-0 basis-1/2 pl-2">
           <input
             type="number"
             aria-label="Top-right corner"
             value={borderRadiusValues['top-right']}
             name="top-right"
             onChange={handleBorderRadiusChange}
-            className="outline-focus w-full max-w-full rounded-sm bg-main p-1 text-xl tracking-wider text-secondary"
+            className="outline-focus w-full max-w-full rounded-l-sm rounded-r-none border-r border-dashed border-secondary bg-main p-1 text-xl tracking-wider text-secondary"
+          />
+          <UnitDropdown
+            aria-label="Top-right corner unit"
+            name="top-right-unit"
+            onChange={handleBorderRadiusUnitChange}
+            value={borderRadiusUnits['top-right-unit']}
+            className="rounded-l-none rounded-r-sm border-l border-dashed border-secondary bg-main p-1 text-xl tracking-wider text-secondary"
           />
         </div>
       </div>
       {children}
       <div className="mx-auto flex w-full max-w-[432px] justify-between overflow-hidden sm:justify-center sm:gap-36">
-        <div className="max-w-36 shrink-0 grow-0 basis-1/2 pr-2">
+        <div className="flex max-w-36 shrink-0 grow-0 basis-1/2 pr-2">
           <input
             type="number"
             aria-label="Bottom-left corner"
             value={borderRadiusValues['bottom-left']}
             name="bottom-left"
             onChange={handleBorderRadiusChange}
-            className="outline-focus w-full max-w-full rounded-sm bg-main p-1 text-xl tracking-wider text-secondary"
+            className="outline-focus w-full max-w-full rounded-l-sm rounded-r-none border-r border-dashed border-secondary bg-main p-1 text-xl tracking-wider text-secondary"
+          />
+          <UnitDropdown
+            aria-label="Bottom-left corner unit"
+            name="bottom-left-unit"
+            onChange={handleBorderRadiusUnitChange}
+            value={borderRadiusUnits['bottom-left-unit']}
+            className="rounded-l-none rounded-r-sm border-l border-dashed border-secondary bg-main p-1 text-xl tracking-wider text-secondary"
           />
         </div>
-        <div className="max-w-36 shrink-0 grow-0 basis-1/2 pl-2">
+        <div className="flex max-w-36 shrink-0 grow-0 basis-1/2 pl-2">
           <input
             type="number"
             aria-label="Bottom-right corner"
             value={borderRadiusValues['bottom-right']}
             name="bottom-right"
             onChange={handleBorderRadiusChange}
-            className="outline-focus w-full max-w-full rounded-sm bg-main p-1 text-xl tracking-wider text-secondary"
+            className="outline-focus w-full max-w-full rounded-l-sm rounded-r-none border-r border-dashed border-secondary bg-main p-1 text-xl tracking-wider text-secondary"
+          />
+          <UnitDropdown
+            aria-label="Bottom-right corner unit"
+            name="bottom-right-unit"
+            onChange={handleBorderRadiusUnitChange}
+            value={borderRadiusUnits['bottom-right-unit']}
+            className="rounded-l-none rounded-r-sm border-l border-dashed border-secondary bg-main p-1 text-xl tracking-wider text-secondary"
           />
         </div>
       </div>
-      <UnitDropdown
-        aria-label="Top-left corner unit"
-        name="top-left-unit"
-        onChange={handleBorderRadiusUnitChange}
-        value={borderRadiusUnits['top-left-unit']}
-      />
-      <UnitDropdown
-        aria-label="Top-right corner unit"
-        name="top-right-unit"
-        onChange={handleBorderRadiusUnitChange}
-        value={borderRadiusUnits['top-right-unit']}
-      />
-      <UnitDropdown
-        aria-label="Bottom-left corner unit"
-        name="bottom-left-unit"
-        onChange={handleBorderRadiusUnitChange}
-        value={borderRadiusUnits['bottom-left-unit']}
-      />
-      <UnitDropdown
-        aria-label="Bottom-right corner unit"
-        name="bottom-right-unit"
-        onChange={handleBorderRadiusUnitChange}
-        value={borderRadiusUnits['bottom-right-unit']}
-      />
     </>
   );
 };
