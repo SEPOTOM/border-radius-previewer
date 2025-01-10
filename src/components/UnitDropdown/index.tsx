7a3f0210f6773a99294dedacf71152709f6b21bf
Artyom Pizin <artempetr2016@gmail.com>
2025-01-10 15:08:42 +0300

    feat(UnitDropdown): display options with border radius measurement units

--- src/components/UnitDropdown/index.tsx
+++ src/components/UnitDropdown/index.tsx
@@ -1,7 +1,17 @@
 import { FC } from 'react';
 
+import { BORDER_RADIUS_UNITS } from '@/utils';
+
 import { UnitDropdownProps } from './types';
 
-const UnitDropdown: FC<UnitDropdownProps> = (props) => <select {...props} />;
+const UnitDropdown: FC<UnitDropdownProps> = (props) => (
+  <select {...props}>
+    {BORDER_RADIUS_UNITS.map((unit, index) => (
+      <option key={index} value={unit}>
+        {unit}
+      </option>
+    ))}
+  </select>
+);
 
 export default UnitDropdown;
