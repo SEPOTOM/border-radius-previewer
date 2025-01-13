88052f5b00849a6308d694f4dd61cac13c2d6206
Artyom Pizin <artempetr2016@gmail.com>
2025-01-13 14:52:33 +0300

    refactor(tests): move tests related to normalMode from general file to normalMode file

--- src/tests/App/general.test.tsx
+++ src/tests/App/general.test.tsx
@@ -4,12 +4,6 @@ import App from '@/App';
 import { renderWithUser } from '@/tests/utils';
 
 describe('App', () => {
-  it('should show the preview box to which the border-radius is applied', () => {
-    render(<App />);
-
-    expect(screen.getByLabelText(/preview box/i)).toBeInTheDocument();
-  });
-
   it('should show the mode switch', () => {
     render(<App />);
 
@@ -28,39 +22,6 @@ describe('App', () => {
     render(<App />);
 
     expect(screen.getByRole('status', { name: /output/i })).toBeInTheDocument();
-  });
-
-  it('should copy the value of the border radius to the clipboard after clicking the copy button', async () => {
-    const { user } = renderWithUser(<App />);
-    const writeTextSpy = vi.spyOn(navigator.clipboard, 'writeText');
-
-    await user.type(
-      screen.getByRole('spinbutton', { name: /top-left/i }),
-      '56',
-    );
-    await user.type(
-      screen.getByRole('spinbutton', { name: /top-right/i }),
-      '98',
-    );
-    await user.type(
-      screen.getByRole('spinbutton', { name: /bottom-left/i }),
-      '10',
-    );
-
-    await user.click(
-      screen.getByRole('button', { name: /copy to clipboard/i }),
-    );
-
-    expect(writeTextSpy).toHaveBeenCalledTimes(1);
-    expect(writeTextSpy).toHaveBeenCalledWith('56px 98px 0 10px');
-  });
-
-  it("shouldn't round the preview box by default", () => {
-    render(<App />);
-    const previewBox = screen.getByLabelText(/preview box/i);
-    const previewBoxStyle = getComputedStyle(previewBox);
-
-    expect(previewBoxStyle.borderRadius).toBe('0 0 0 0');
   });
 
   it('the mode switch should be turned off by default', () => {
--- src/tests/App/normalMode.test.tsx
+++ src/tests/App/normalMode.test.tsx
@@ -5,6 +5,45 @@ import { renderWithUser } from '@/tests/utils';
 import { BORDER_RADIUS_UNITS, ORDERED_CORNERS } from '@/utils';
 
 describe('App in normal mode', () => {
+  it('should show the preview box to which the border-radius is applied', () => {
+    render(<App />);
+
+    expect(screen.getByLabelText(/preview box/i)).toBeInTheDocument();
+  });
+
+  it('should copy the value of the border radius to the clipboard after clicking the copy button', async () => {
+    const { user } = renderWithUser(<App />);
+    const writeTextSpy = vi.spyOn(navigator.clipboard, 'writeText');
+
+    await user.type(
+      screen.getByRole('spinbutton', { name: /top-left/i }),
+      '56',
+    );
+    await user.type(
+      screen.getByRole('spinbutton', { name: /top-right/i }),
+      '98',
+    );
+    await user.type(
+      screen.getByRole('spinbutton', { name: /bottom-left/i }),
+      '10',
+    );
+
+    await user.click(
+      screen.getByRole('button', { name: /copy to clipboard/i }),
+    );
+
+    expect(writeTextSpy).toHaveBeenCalledTimes(1);
+    expect(writeTextSpy).toHaveBeenCalledWith('56px 98px 0 10px');
+  });
+
+  it("shouldn't round the preview box by default", () => {
+    render(<App />);
+    const previewBox = screen.getByLabelText(/preview box/i);
+    const previewBoxStyle = getComputedStyle(previewBox);
+
+    expect(previewBoxStyle.borderRadius).toBe('0 0 0 0');
+  });
+
   it('should keep the rounding after switching to advanced mode', async () => {
     const values = ['31', '52', '89', '90'];
     const { user } = renderWithUser(<App />);
